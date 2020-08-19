3a29b5e8f61081d9985d6ba3ad8ed01b8b0f9b28
Eric Sorenson <eric@puppet.com>
2020-08-19 13:00:09 -0700

    Add support for PULUMI_CI, fix quoting in clone

--- steps/pulumi-step-run/pulumi-entrypoint.sh
+++ steps/pulumi-step-run/pulumi-entrypoint.sh
@@ -11,7 +11,6 @@ PULUMI_BACKEND_URL=$(ni get -p {.pulumi_backend_url})
 # secret, needed to login
 PULUMI_ACCESS_TOKEN=$(ni get -p {.pulumi_access_token})
 # for future use - these mimic running against a PR for CI
-PULUMI_CI=
 PULUMI_ROOT=
 GITHUB_WORKFLOW=
 GITHUB_EVENT_PATH=/github/workflow/event.json
@@ -31,9 +30,11 @@ NPM_AUTH_TOKEN=
 PULUMI_ARGS=$(ni get -p {.pulumi_commandline})
 GITHUB_TOKEN=$(ni get -p {.github_token})
 GITHUB_WORKSPACE=/workspace
+# if we're operating on a pull request, set PULUMI_CI=pr
+[[ $(jq -e .pull_request.number $GITHUB_EVENT_PATH) ]] && PULUMI_CI=pr
 
 # ultra-simple copy of actions/checkout@v2 
-git clone $(jq .repository.ssh_url $GITHUB_EVENT_PATH) ${GITHUB_WORKSPACE}
-cd ${GITHUB_WORKSPACE}
+git clone $(jq -r .repository.ssh_url $GITHUB_EVENT_PATH) ${GITHUB_WORKSPACE}
+cd ${GITHUB_WORKSPACE} || (echo "could not find repo directory" && exit 1)
 
 /usr/bin/pulumi-action ${PULUMI_ARGS}
